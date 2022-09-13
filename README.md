5ef2f656fec1b296ea212ae44cba876d2aa1ed84
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-09-13 00:21:20 -0400

    added cli name, password option handling

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ basic script to generate qr code based on given user input
 3. Install Required Node Modules
     - `npm install`
 4. Start script
-    - `npm start`
+    - `node index.js`
 5. Input Wifi SSID (name) & Password
+    - you may also pass the above as options with the start script: `--name YOUR_WIFI_NAME --password YOUR_WIFI_PASSWORD`
 6. Scan resulting QR code with any smart device's camera & join desired network!
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const prompt = require('prompt');
 const qrcode = require('wifi-qr-code-generator');
 const process = require('process');
+const colors = require('@colors/colors/safe');
+const NAME_INPUT_ERROR_MESSAGE = 'Name must be only letters, spaces, dashes, or single quotes';
+// Skip first 2 elements in process.argv array to grab any - or -- options passed
+const argv = require('minimist')(process.argv.slice(2));
 /**
  * Define Input Schema
  */
 const schema = {
   properties: {
     name: {
+      description: colors.brightGreen.bgBlack('Enter network name'),
       pattern: /^[a-zA-Z0-9\s\-\']+$/,
-      message: 'Name must be only letters, spaces, dashes, or single quotes',
+      message: colors.zalgo(NAME_INPUT_ERROR_MESSAGE),
       required: true,
     },
     password: {
+      description: colors.brightGreen.bgBlack('Enter network password'),
       hidden: true,
     },
   },
@@ -23,14 +29,14 @@ const schema = {
  * @param {{ name, password }} result the user input for wifi name & pwd
  * @returns {any} exit code based on result & any errors
  */
-async function handlePromptInput(error, result) {
+async function handlePromptInput(error, result = argv) {
   if (error) {
-    console.warn('error: ', error);
+    console.warn('error: ', colors.zalgo(error));
     return process.exit(1);
   }
   try {
     console.clear();
-    console.log(`${result.name} Login:\n`)
+    console.log(colors.rainbow(`\n\n\n${result.name} Login:\n`));
     console.log(await qrcode.generateWifiQRCode({
       ssid: result.name,
       password: result.password,
@@ -40,10 +46,16 @@ async function handlePromptInput(error, result) {
     }));
     return process.exit(0);
   } catch (er) {
-    console.error('qr code error: ', er);
+    console.error(colors.zalgo('qr code error: '), er);
     return process.exit(1);
   }
 };
+/**
+ * Prompt Options
+ */
+prompt.message = '';
+prompt.delimiter = colors.brightGreen.bgBlack(':');
+prompt.override = argv;
 /**
  * Start prompt
  */
